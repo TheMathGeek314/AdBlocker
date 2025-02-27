7e832d036a05873575af374abe939e8af49983d6
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2025-02-27 16:06:04 -0600

    Update Satchel version

--- AdBlocker.cs
+++ AdBlocker.cs
@@ -22,10 +22,10 @@ namespace AdBlocker {
             orig(self);
             //gruz mother
             if(self.gameObject.name == "Battle Scene" && self.FsmName == "Battle Control" && self.gameObject.scene.name == "Crossroads_04") {
-                ((SetIntValue)self.GetState("Start").Actions[0]).intValue = 1;
+                ((SetIntValue)self.GetValidState("Start").Actions[0]).intValue = 1;
             }
             else if(self.gameObject.name == "Corpse Big Fly Burster(Clone)" && self.FsmName == "burster") {
-                FsmState spawnFlies2 = self.GetState("Spawn Flies 2");
+                FsmState spawnFlies2 = self.GetValidState("Spawn Flies 2");
                 spawnFlies2.RemoveAction(1);
                 SetIntValue endGruzFight = new();
                 endGruzFight.intVariable = self.FsmVariables.GetFsmInt("Battle Enemies");
@@ -40,76 +40,76 @@ namespace AdBlocker {
             }
             //false knight, failed champ
             else if(self.gameObject.name.Contains("False Knight") && self.FsmName == "FalseyControl") {
-                self.GetState("S Attack Recover").RemoveAction(2);
-                self.GetState("Turn").RemoveAction(0);
+                self.GetValidState("S Attack Recover").RemoveAction(2);
+                self.GetValidState("Turn").RemoveAction(0);
                 for(int i = self.gameObject.name.Contains("Dream") ? 5 : 3; i >= 3; i--) {
-                    self.GetState("JA Recoil").RemoveAction(i);
+                    self.GetValidState("JA Recoil").RemoveAction(i);
                 }
             }
             //hornet 2
             else if(self.gameObject.name == "Hornet Boss 2" & self.FsmName == "Control") {
-                self.GetState("Barb Throw").RemoveAction(1);
+                self.GetValidState("Barb Throw").RemoveAction(1);
             }
             //flukemarm
             else if(self.gameObject.name == "Fluke Mother" && self.FsmName == "Fluke Mother") {
                 for(int i = 7; i >= 4; i--) {
-                    self.GetState("Spawn 2").RemoveAction(i);
-                    self.GetState("Spawn").RemoveAction(i);
+                    self.GetValidState("Spawn 2").RemoveAction(i);
+                    self.GetValidState("Spawn").RemoveAction(i);
                 }
             }
             //hive knight
             else if(self.gameObject.name == "Battle Scene" && self.FsmName == "Control" && self.gameObject.scene.name == "Hive_05") {
-                self.GetState("Droppers").RemoveAction(2);
+                self.GetValidState("Droppers").RemoveAction(2);
             }
             else if(self.gameObject.name == "Hive Knight" && self.FsmName == "Control") {
-                self.GetState("Glob Strike").RemoveAction(3);
-                self.GetState("Roar Recover").RemoveAction(0);
+                self.GetValidState("Glob Strike").RemoveAction(3);
+                self.GetValidState("Roar Recover").RemoveAction(0);
             }
             //broken vessel, lost kin
             else if((self.gameObject.name == "Infected Knight" || self.gameObject.name == "Lost Kin") && self.FsmName == "Spawn Balloon") {
-                self.GetState("Spawn").RemoveAction(9);
-                self.GetState("Spawn").RemoveAction(8);
+                self.GetValidState("Spawn").RemoveAction(9);
+                self.GetValidState("Spawn").RemoveAction(8);
             }
             //winged nosk
             else if(self.gameObject.name == "Hornet Nosk" && self.FsmName == "Hornet Nosk") {
-                self.GetState("Summon").RemoveAction(7);
-                self.GetState("Summon").RemoveAction(6);
+                self.GetValidState("Summon").RemoveAction(7);
+                self.GetValidState("Summon").RemoveAction(6);
             }
             //collector
             else if(self.gameObject.name == "Jar Collector" && self.FsmName == "Control") {
                 FsmEvent skipEvent = new FsmEvent("SKIP SUMMON");
                 self.AddTransition("Spawn", "SKIP SUMMON", "Spawn Recover");
-                self.GetState("Spawn").InsertAction(new justSendEvent(skipEvent), 1);
+                self.GetValidState("Spawn").InsertAction(new justSendEvent(skipEvent), 1);
             }
             //uumuu
             else if(self.gameObject.name == "Mega Jellyfish GG" && self.FsmName == "Mega Jellyfish") {
-                self.GetState("Roar").RemoveAction(3);
+                self.GetValidState("Roar").RemoveAction(3);
             }
             //zote
             else if(self.gameObject.name == "Grey Prince" && self.FsmName == "Control") {
                 self.ChangeTransition("Spit Dir", "L", "Spit Recover");
                 self.ChangeTransition("Spit Dir", "R", "Spit Recover");
-                self.GetState("B Roar").RemoveAction(4);
+                self.GetValidState("B Roar").RemoveAction(4);
             }
             //soul warrior
             else if(self.FsmName == "Battle Control" && (self.gameObject.scene.name == "Ruins1_31b" || self.gameObject.scene.name.Contains("Mage_Knight"))) {
-                self.GetState("Spawn").RemoveAction(1);
+                self.GetValidState("Spawn").RemoveAction(1);
             }
             //no eyes
             else if(self.gameObject.name == "Ghost Warrior No Eyes" && self.FsmName == "Shot Spawn") {
                 foreach(int i in new int[] { 6, 4, 2, 0 }){
-                    self.GetState("Spawn L").RemoveAction(i);
-                    self.GetState("Spawn R").RemoveAction(i);
+                    self.GetValidState("Spawn L").RemoveAction(i);
+                    self.GetValidState("Spawn R").RemoveAction(i);
                 }
             }
             //galien
             else if(self.gameObject.name == "Ghost Warrior Galien" && self.FsmName == "Summon Minis") {
-                self.GetState("Summon").RemoveAction(1);
-                self.GetState("Summon 2").RemoveAction(1);
+                self.GetValidState("Summon").RemoveAction(1);
+                self.GetValidState("Summon 2").RemoveAction(1);
             }
             //markoth
             else if(self.gameObject.name == "Ghost Warrior Markoth" && self.FsmName == "Attacking") {
-                self.GetState("Nail").RemoveAction(0);
+                self.GetValidState("Nail").RemoveAction(0);
             }
             //elder hu
             else if(self.gameObject.name == "Ghost Warrior Hu" && self.FsmName == "Attacking") {
@@ -117,12 +117,12 @@ namespace AdBlocker {
                     self.ChangeTransition("Choice", i.ToString(), "Ring Antic");
                     self.ChangeTransition("Choice 2", i.ToString(), "Ring Antic");
                 }
-                self.GetState("M 1").RemoveAction(0);
+                self.GetValidState("M 1").RemoveAction(0);
                 for(int i = 2; i <= 8; i++) {
-                    self.GetState("M " + i.ToString()).RemoveAction(1);
-                    self.GetState("M " + i.ToString()).RemoveAction(0);
+                    self.GetValidState("M " + i.ToString()).RemoveAction(1);
+                    self.GetValidState("M " + i.ToString()).RemoveAction(0);
                 }
-                self.GetState("M 9").RemoveAction(0);
+                self.GetValidState("M 9").RemoveAction(0);
             }
         }
     }
